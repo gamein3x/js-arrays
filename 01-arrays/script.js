53563996637a05ab119b1da9aad4584fdc4667fd
Ivan Caldarella <gamein3x@gmail.com>
2026-03-25 16:39:48 +0100

    Primo esercizio completo, nessun problema

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -1,3 +1,5 @@
+`use strict`;
+
 const teachers = [
   'Nathan',
   'Ed',
@@ -10,24 +12,41 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = null;
+const fourthTeacher = teachers[3];
+console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+let fifthTeacher = teachers[4];
+console.log(fifthTeacher);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+console.log(`Rimosso ${lastTeacher}`);
+console.log(teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log(`Rimosso ${firstTeacher}`);
+console.log(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+const teachName0 = 'Vanessa'
+const newTeacher0 = teachers.push(teachName0);
+console.log(`Aggiunta ${teachName0}`);
+console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+const teachName1 = 'Sarah'
+const newTeacher1 = teachers.unshift(teachName1);
+console.log(`Aggiunta ${teachName1}`);
+console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers.indexOf('Lewis');
+console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length === 0;
+console.log(isTeachersEmpty);
